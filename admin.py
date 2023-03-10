5e126c991c07fa6e92515e1d1a13fcb12a7c7993
KablammoMan <codernaffilio@gmail.com>
2023-03-10 21:16:32 +1000

    Update admin.py

--- admin.py
+++ admin.py
@@ -7,8 +7,51 @@ detaB = deta.Deta(config.api_key)
 db = detaB.Base("command")
 
 
+def edit_property(data, prop, key):
+    accept = False
+    while not accept:
+        os.system("cls")
+        print("#" * 20)
+        print()
+        print("Edit Property Menu")
+        print()
+        print(f"Selected Computer: {key}")
+        print(f"Selected Property: {prop}")
+        print("\n")
+        accept = True
+        choice = input(f"Choose Value for '{prop}' of '{key}' (0/1): ")
+        if not choice in ["0", "1"]:
+            accept = False
+    data[prop] = int(choice)
+    db.put(data, key)
+    main()
+
+
 def edit_computer(data):
-    pass
+    accept = False
+    while not accept:
+        os.system("cls")
+        print("#" * 20)
+        print()
+        print("Computer Properties Menu")
+        print()
+        print(f"Selected Computer: {data['key']}")
+        print("\n")
+        compData = {}
+        i = 1
+        for k,v in data.items():
+            if k != "key":
+                compData[k] = v
+                print(f"{i}. {k}: {v}")
+                print()
+        print()
+        accept = True
+        choice = input("Choose a Property Number: ")
+        try:
+            selectedProp = compData.keys()[int(choice) - 1]
+        except:
+            accept = False
+    edit_property(compData, selectedProp, data["key"])
 
 
 def delete_computer(key):   
@@ -44,7 +87,7 @@ def computer_management(data):
         print("3. Return to Main Menu")
         print("\n")
         accept = True
-        choice = input("Choose an option: ")
+        choice = input("Choose an Option Number: ")
         if not choice in ["1", "2", "3"]:
             accept = False
     if choice == "1":
